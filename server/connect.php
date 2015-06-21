4bba3c4a748e7b7e4949832ede4b2dd3bee8d199
jein.yi <jein.yi@nhnent.com>
2015-06-21 23:55:28 +0900

    server info remove

--- server/connect.php
+++ server/connect.php
@@ -13,11 +13,11 @@ $error = $config['ERROR'];
 $code = $config['CODE'];
 $name = $config['NAME'];
 
-$db_server_ip = $info['SERVER_IP'];// "121.124.124.238";
-$db_server = $info['SERVER']; // "121.124.124.238";
-$db_user = $info['USER']; // "dltjsejr";
-$db_pwd = $info['PASSWORD']; //'tjsejr12850408!#$';
-$db_name = $info['DBNAME']; // "bb1322";]
+$db_server_ip = $info['SERVER_IP'];
+$db_server = $info['SERVER'];
+$db_user = $info['USER'];
+$db_pwd = $info['PASSWORD'];
+$db_name = $info['DBNAME'];
 
 $db = new mysqli($db_server, $db_user,$db_pwd, $db_name);
 // error
